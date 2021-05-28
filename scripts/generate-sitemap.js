6b4d6390040632d0f91161420a83913e416100a1
Starriv <starriv.993@gmail.com>
2021-05-28 18:22:44 +0800

    Update generate-sitemap file

--- scripts/generate-sitemap.js
+++ scripts/generate-sitemap.js
@@ -28,7 +28,7 @@ const prettier = require('prettier');
 
                 return `
                         <url>
-                            <loc>${`https://leerob.io${route}`}</loc>
+                            <loc>${`https://starriv.com${route}`}</loc>
                         </url>
                     `;
               })
