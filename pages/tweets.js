1a43269a010d524f97dcb4dcc0a5bc8c780b6851
Starriv <starriv.9993@gmail.com>
2021-05-28 17:09:54 +0800

    Temporarily remove tweets

--- pages/tweets.js
+++ pages/tweets.js
@@ -1,11 +1,11 @@
 import Container from '@/components/Container';
-import Tweet from '@/components/Tweet';
-import { getTweets } from '@/lib/twitter';
+// import Tweet from '@/components/Tweet';
+// import { getTweets } from '@/lib/twitter';
 
-export default function Tweets({ tweets }) {
+export default function Tweets() {
   return (
     <Container
-      title="Tweets – Lee Robinson"
+      title="Tweets – Starriv"
       description="A collection of tweets that inspire me, make me laugh, and make me think."
     >
       <div className="flex flex-col justify-center items-start max-w-2xl mx-auto mb-16">
@@ -17,37 +17,38 @@ export default function Tweets({ tweets }) {
           bit, so I wanted a place to publicly share what inspires me, makes me
           laugh, and makes me think.
         </p>
-        {tweets.map((tweet) => (
+        {/* {tweets.map((tweet) => (
           <Tweet key={tweet.id} {...tweet} />
-        ))}
+        ))} */}
       </div>
     </Container>
   );
 }
 
 export async function getStaticProps() {
-  const tweets = await getTweets([
-    '1189444653059174401',
-    '935857414435495937',
-    '1334528781139259400',
-    '1334334544598740994',
-    '826528907381739520',
-    '1308509070140162048',
-    '997895977179721728',
-    '1341090253864542208',
-    '1383873047619276812',
-    '1026872652290379776',
-    '1346113149112619016',
-    '1340107217970683906',
-    '992629481578745856',
-    '989142253468708864',
-    '807626710350839808',
-    '1341962177272537089',
-    '1342869937841266688',
-    '1116362674319908864',
-    '1331380003716681728',
-    '1002104154737684480'
-  ]);
+  // const tweets = await getTweets([
+  //   '1189444653059174401',
+  //   '935857414435495937',
+  //   '1334528781139259400',
+  //   '1334334544598740994',
+  //   '826528907381739520',
+  //   '1308509070140162048',
+  //   '997895977179721728',
+  //   '1341090253864542208',
+  //   '1383873047619276812',
+  //   '1026872652290379776',
+  //   '1346113149112619016',
+  //   '1340107217970683906',
+  //   '992629481578745856',
+  //   '989142253468708864',
+  //   '807626710350839808',
+  //   '1341962177272537089',
+  //   '1342869937841266688',
+  //   '1116362674319908864',
+  //   '1331380003716681728',
+  //   '1002104154737684480'
+  // ]);
 
-  return { props: { tweets } };
+  // return { props: { tweets } };
+  return { props: {  } }
 }
